6f115c6f2ee34f48b15bb9763a139795826e01af
satyanarayandinda3-jpg <satyanarayandinda3@gmail.com>
2026-03-01 21:06:08 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'DOREMON',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "D O R E M O N",
+  botOwner: 'SATYA', // Your name
+  ownerNumber: '918293255304', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
